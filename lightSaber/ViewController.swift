9e52ebb1c12bdcfccf6eda97823f99f06db09890
Paul Nicholsen <pnichols104@gmail.com>
2016-03-02 21:52:11 -0500

    Initial Commit

---
+++ lightSaber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lightSaber
+//
+//  Created by Paul Nichols on 3/2/16.
+//  Copyright © 2016 Paul Nicholsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
