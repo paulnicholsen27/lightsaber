65ab599d2fd8112cfd640d58a603bbf7162b1e74
Paul Nicholsen <pnichols104@gmail.com>
2016-03-10 23:00:48 -0500

    Light saber animates and makes sounds

--- lightSaber/ViewController.swift
+++ lightSaber/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainImage: UIImageView!
+    var timer = NSTimer()
+    var counter = 1
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let saberOnPath = NSBundle.mainBundle().pathForResource("coolsaber", ofType: ".wav")
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(string: saberOnPath!)!)
+        } catch let error as NSError {
+            print(error)
+        }
+        player.play()
+        timer = NSTimer.scheduledTimerWithTimeInterval(
+            0.1,
+            target: self,
+            selector: "initialAnimation",
+            userInfo: nil,
+            repeats: true)
     }
 
+    func initialAnimation() {
+        if counter == 10 {
+            timer.invalidate()
+        } else {
+            var nextImage = "saber\(counter).png"
+            mainImage.image = UIImage(named: nextImage)
+            counter++
+            
+        }
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent!) {
+        if event.subtype == UIEventSubtype.MotionShake {
+            playSound()
+        }
+    }
+    func playSound() {
+        let random = Int(arc4random_uniform(5) + 1)
+        print(random)
+        let saberOnPath = NSBundle.mainBundle().pathForResource("swing\(random)", ofType: ".wav")
+        print(saberOnPath)
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(string: saberOnPath!)!)
+        } catch let error as NSError {
+            print(error)
+        }
+        player.play()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
